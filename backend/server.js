0c711e8f23ed672466c89fd1768725794ce6450f
Your Name <you@example.com>
2026-04-05 13:27:23 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,20 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/mongodb.js";
 import "dotenv/config";
-
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import expenseRouter from "./routes/expenseRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
-
 connectDB();
 
-app.use(
-  cors({
-    credentials: true,
-  }),
-);
-
 app.use(express.json());
+app.use(cors({ credentials: true }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+
 app.use("/api/expenses", expenseRouter);
 
 app.listen(port, () => {
