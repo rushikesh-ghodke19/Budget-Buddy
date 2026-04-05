21c0a7f9be6df4ecacdb1b871086ddb5e1c8cefd
Your Name <you@example.com>
2026-04-05 21:34:46 +0530

    Debugging code

--- backend/config/sendEmail.js
+++ backend/config/sendEmail.js
@@ -1,10 +1,9 @@
 import nodemailer from "nodemailer";
 
 const transpoter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
