068ec1c305c5b89ed1334eaa03592ff604b4852b
Your Name <you@example.com>
2026-04-07 19:58:27 +0530

    Debugging code

--- backend/config/sendEmail.js
+++ backend/config/sendEmail.js
@@ -1,9 +1,7 @@
 import nodemailer from "nodemailer";
 
 const transpoter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  host: "gmail",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
